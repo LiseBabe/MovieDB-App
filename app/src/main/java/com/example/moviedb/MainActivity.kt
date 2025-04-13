11647985db046ffd6dda692b0ef7804c29ab380c
elliotdelano <75746725+elliotdelano@users.noreply.github.com>
2025-04-13 21:05:12 +0200

    Merged the screens the genres and the imdb link.

--- app/src/main/java/com/example/moviedb/MainActivity.kt
+++ app/src/main/java/com/example/moviedb/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.moviedb
 
 import android.content.Context
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -18,8 +18,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -29,12 +34,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.pointer.motionEventSpy
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -174,7 +178,7 @@ fun HomeScreen(navController: NavHostController) {
 @Composable
 fun GenreList(genreList: List<Genre>, modifier: Modifier = Modifier){
     LazyVerticalGrid (columns = GridCells.Fixed(3), modifier = modifier) {
-        items(genreList){ genre ->
+        items(genreList) { genre ->
             GenreListItemCard(genre, modifier.padding(8.dp))
         }
     }
