fdb5ee1d57190865247bf14b4e1bdedbf1348a76
elliotdelano <75746725+elliotdelano@users.noreply.github.com>
2025-04-13 12:55:18 +0200

    Added an info button to the movie card. This buttons opens the imdb page/app for that movie.

--- app/src/main/java/com/example/moviedb/MainActivity.kt
+++ app/src/main/java/com/example/moviedb/MainActivity.kt
@@ -1,28 +1,41 @@
 package com.example.moviedb
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.expandHorizontally
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.pointer.motionEventSpy
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -31,6 +44,7 @@ import com.example.moviedb.ui.theme.MovieDBTheme
 import com.example.moviedb.models.Movie
 import com.example.moviedb.utils.Constans
 import coil.compose.AsyncImage
+import androidx.core.net.toUri
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -51,7 +65,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MovieDBApp(name: String, modifier: Modifier = Modifier) {
-    MovieList(movieList = Movies().getMovies(), )
+    MovieList(movieList = Movies().getMovies())
 }
 
 @Composable
@@ -67,6 +81,7 @@ fun MovieList(movieList: List<Movie>, modifier: Modifier = Modifier){
 
 @Composable
 fun MovieListItemCard(movie: Movie, modifier: Modifier = Modifier){
+    val context = LocalContext.current;
     Card (modifier = modifier){
         Row {
             Box {
@@ -78,8 +93,24 @@ fun MovieListItemCard(movie: Movie, modifier: Modifier = Modifier){
                 )
             }
             Column {
-                Text(text = movie.title,
-                    style = MaterialTheme.typography.headlineSmall)
+                Row (
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.Top,
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ){
+                    Text(text = movie.title,
+                        style = MaterialTheme.typography.headlineSmall,
+                        modifier = Modifier.weight(1f))
+                    IconButton(
+                        onClick = {
+                        openIMDB(context, movie.imdb_id)
+                    }) {
+                        Icon(
+                            imageVector = Icons.Filled.Info,
+                            contentDescription = "IMDB Link"
+                        )
+                    }
+                }
                 Spacer(modifier = Modifier.size(8.dp))
 
                 Text(text = movie.releaseDate,
@@ -103,4 +134,13 @@ fun GreetingPreview() {
     MovieDBTheme {
         MovieDBApp("Android")
     }
+}
+
+private fun openIMDB(context: Context, id: String) {
+    //https://developer.android.com/guide/components/intents-filters
+    val intent = Intent(Intent.ACTION_VIEW).apply {
+        data = ("https://imdb.com/title/$id").toUri()
+    }
+    context.startActivity(Intent.createChooser(intent, "Open in IMDB"))
+
 }
