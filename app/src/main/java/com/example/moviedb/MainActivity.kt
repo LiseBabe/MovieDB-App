a3573dd066f7ad69bc803d1bf95cdf9dfbc25a81
Thomas_chbn <147448902+Darkman34@users.noreply.github.com>
2025-04-13 19:23:23 +0200

    3 screens made

--- app/src/main/java/com/example/moviedb/MainActivity.kt
+++ app/src/main/java/com/example/moviedb/MainActivity.kt
@@ -4,28 +4,38 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.pointer.motionEventSpy
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.moviedb.database.Movies
 import com.example.moviedb.ui.theme.MovieDBTheme
 import com.example.moviedb.models.Movie
@@ -51,7 +61,12 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MovieDBApp(name: String, modifier: Modifier = Modifier) {
-    MovieList(movieList = Movies().getMovies(), )
+    val navController = rememberNavController()
+    NavHost(navController, startDestination = "home") {
+        composable("home") { HomeScreen(navController) }
+        composable("second") { SecondScreen(navController) }
+        composable("third") { ThirdScreen(navController) }
+    }
 }
 
 @Composable
@@ -97,10 +112,131 @@ fun MovieListItemCard(movie: Movie, modifier: Modifier = Modifier){
     }
 }
 
+@Composable
+fun HomeScreen(navController: NavHostController) {
+    Scaffold { innerPadding ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding),
+            contentAlignment = Alignment.Center
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(24.dp)
+            ) {
+                Text(
+                    text = "Welcome to MovieDB",
+                    style = MaterialTheme.typography.headlineLarge
+                )
+
+                Button(
+                    onClick = { navController.navigate("second") },
+                    modifier = Modifier.fillMaxWidth(0.8f)
+                ) {
+                    Text("List of Movies")
+                }
+
+                Button(
+                    onClick = { navController.navigate("third") },
+                    modifier = Modifier.fillMaxWidth(0.8f)
+                ) {
+                    Text("Movie Links")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun SecondScreen(navController: NavHostController) {
+    Scaffold(
+        bottomBar = {
+            Button(
+                onClick = { navController.popBackStack() },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                Text("Home")
+            }
+        }
+    ) { innerPadding ->
+        Column(modifier = Modifier
+            .fillMaxSize()
+            .padding(innerPadding)) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 16.dp)
+            ) {
+                Text(
+                    text = "Movie List",
+                    style = MaterialTheme.typography.headlineMedium,
+                    modifier = Modifier.align(Alignment.Center)
+                )
+            }
+
+            MovieList(movieList = Movies().getMovies())
+        }
+    }
+}
+
+@Composable
+fun ThirdScreen(navController: NavHostController) {
+    Scaffold(
+        bottomBar = {
+            Button(
+                onClick = { navController.popBackStack() },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                Text("Home")
+            }
+        }
+    ) { innerPadding ->
+        Column(modifier = Modifier
+            .fillMaxSize()
+            .padding(innerPadding)) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 16.dp)
+            ) {
+                Text(
+                    text = "Movie Links",
+                    style = MaterialTheme.typography.headlineMedium,
+                    modifier = Modifier.align(Alignment.Center)
+                )
+            }
+        }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MovieDBTheme {
-        MovieDBApp("Android")
+        val navController = rememberNavController()
+        HomeScreen(navController = navController)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview2() {
+    MovieDBTheme {
+        val navController = rememberNavController()
+        SecondScreen(navController = navController)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview3() {
+    MovieDBTheme {
+        val navController = rememberNavController()
+        ThirdScreen(navController = navController)
     }
 }
